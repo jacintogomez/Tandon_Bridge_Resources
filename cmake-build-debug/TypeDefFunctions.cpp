06bb30bb569b49de23f0125b5174f95266c067a3
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-03-08 13:50:04 -0500

    Typedef

--- cmake-build-debug/TypeDefFunctions.cpp
+++ cmake-build-debug/TypeDefFunctions.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 //We need to define a new type
 typedef int (*ptrfunc)(int);
+typedef void (*msgfunc)();
 
 int addone(int x){
     return x+1;
@@ -17,9 +18,20 @@ ptrfunc dotask(){
     return addone; //The function returned here is addone
 }
 
+void m1(){cout<<"Hello ";}
+void m2(){cout<<"how ";}
+void m3(){cout<<"are you?";}
+
 int main(){
     //Define a new pointer function ptrf, which is defined as the dotask() function
     ptrfunc ptrf=dotask();
-    cout<<ptrf(100);
+    cout<<ptrf(100)<<endl;
+    //^Very simple example
+
+    //More useful application: creating a list of functions to call all of them
+    msgfunc functionarray[]={m1,m2,m3};
+    for(msgfunc printthis:functionarray){
+        printthis();
+    }
     return 0;
 }
