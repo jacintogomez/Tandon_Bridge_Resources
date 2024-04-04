075d1d5efd6a9d85d5c8258f90e87328bf2fbd78
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-04-04 02:37:47 -0400

    Adding tricks

--- tricks.cpp
+++ tricks.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+//this class is for the auto example just ignore it
+class ComplexThing{
+public:
+    ComplexThing(int n):member(n){}
+    int member;
+};
+
 int main(){
-    int x=234,y=89;
+    //initializing variables for examples
+    int x=234,y=89,num=345;
+    ComplexThing object(5);
+    vector<ComplexThing> vec1={object,object,object,object};
+
+    //-----------------------------------------------------
+
     cout<<"x is "<<x<<" and y is "<<y<<endl;
     //swap two ints without using a temp variable
     x^=y;
@@ -10,16 +24,33 @@ int main(){
     x^=y;
     cout<<"x is "<<x<<" and y is "<<y<<endl;
 
+    //-----------------------------------------------------
+
     //is number even or odd?
-    int num=345;
-    (num%2==0)?cout<<"even"<<endl:cout<<"even"<<endl;
+    (num%2==0)?cout<<"even"<<endl:cout<<"even"<<endl; //<- ternary operator
     (num&1)?cout<<"even"<<endl:cout<<"even"<<endl;
+
+    //-----------------------------------------------------
 
     //shorter single if statement
     if(num==345){
         cout<<"it is"<<endl;
     }
     num==345&&cout<<"it is"<<endl;
+
+    //-----------------------------------------------------
+
+    //auto keyword
+    ComplexThing* example1=new ComplexThing(7);
+    auto example2=new ComplexThing(8);
+
+    vector<ComplexThing*> vec2={example1,example2};
+    for(ComplexThing* element:vec2){
+        cout<<element->member<<" ";
+    }cout<<endl;
+    for(auto element:vec2){
+        cout<<element->member<<" ";
+    }cout<<endl;
 
     return 0;
 }
